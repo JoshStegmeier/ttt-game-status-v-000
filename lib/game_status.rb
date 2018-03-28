39c2096d64c423c9988bb5e3a0970e11d64cdd7f
Josh Stegmeier <josh@steg.me>
2018-03-28 00:20:34 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,6 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo_array|
-    puts "first, list each combo array. #{combo_array}"
     win_index_1 = combo_array[0]
     win_index_2 = combo_array[1]
     win_index_3 = combo_array[2]
