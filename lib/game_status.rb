bd293c0d1af1b45d055586cf771ee4c50f6e0ac6
Josh Stegmeier <josh@steg.me>
2018-03-28 00:35:57 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,9 +27,11 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return combo_array
-    else
-      return false
+      winning_combo = combo_array
     end
   end
+  if winning_combo
+    return winning_combo
+  else
+    return false
 end
