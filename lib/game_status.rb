2cbf600632650ceaef09545a5c91e7cb169a333e
Josh Stegmeier <josh@steg.me>
2018-03-28 01:23:48 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -55,7 +55,9 @@ def draw?(board)
 end
 
 def over?(board)
-  if draw?(board) == true || won?(board) == true
+  if won?(board) == true
+    return true
+  elsif draw?(board) == true
     return true
   else
     return false
